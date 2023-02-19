6e34d87116e47a57e70d865028656885564fb11e
melvingaye <melvind.gaye@gmail>
2023-02-19 13:24:45 -0500

    loading notes via static method

--- Notes/Models/Note.cs
+++ Notes/Models/Note.cs
@@ -11,5 +11,27 @@ namespace Notes.Models
         public string Filename { get; set; }
         public string Text { get; set; }
         public DateTime Date { get; set; }
+
+        public void Save() =>
+            File.WriteAllText(Path.Combine(FileSystem.AppDataDirectory, Filename), Text);
+
+        public void Delete() =>
+            File.Delete(Path.Combine(FileSystem.AppDataDirectory, Filename));
+
+        public static Note Load(string filename)
+        {
+            filename = Path.Combine(FileSystem.AppDataDirectory, filename);
+
+            if (!File.Exists(filename))
+                throw new FileNotFoundException("Unable to find file on local storage.", filename);
+
+            return
+                new()
+                {
+                    Filename = Path.GetFileName(filename),
+                    Text = File.ReadAllText(filename),
+                    Date = File.GetLastWriteTime(filename)
+                };
+        }
     }
 }
